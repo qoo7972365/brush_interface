6740898601c67c80f01c1af982a4757678c85a96
qoo7972365 <32336596+qoo7972365@users.noreply.github.com>
2022-01-10 17:10:37 +0800

    Update brush.py

--- brush.py
+++ brush.py
@@ -1,29 +1,33 @@
-import requests,json
+import requests,json,os
 import urllib3
 import time
 import random
 from concurrent.futures import ThreadPoolExecutor
-
+import datetime
+import re
+from urllib.parse import quote
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def create_user():
-    cookies = {
-        '_csrf_token': '4f1b388d7ca0a2bfca1eba8e23149cc5c18e9d12854fb7aebb4cc97cefb1f86e',
+    get_csrf_cookies = {
+        '_csrf_token': '47f53d5a93beff1aa4ef0753716a48579b9f5bb0dd766f1d3195fcdd72f64ae3',
         'PHPSESSID': '0nojre5l3nonta0mi5escadhrk',
         'my-application-browser-tab': '',
-        'AWSALB': 'SqjCpoUnnoRLXHZj3+bsyVHJA6STF1NkXdQNWmRE4szQRaXxj56OhFjZfuuHqGOXzTxmnPxUHl9SBqlNHMLAzJ8fbsmgseZ9mHQPBggj1SanRSRXgR6Ttvl8xQBX',
+        'AWSALB': 'IGH94uKrN7w5yEVS9uoP97fceyhQOmTCNxtvRtHb1Ng1pJinhHZvD86OVVD4RTt4Ob1okBM5/eKcQfnZ7OHyMXazmggAt1oE+cJCqSoZKTXTdkoKkmnnrHsRt+DX',
     }
     
-    headers = {
+    get_csrf_headers = {
+        'X-Forwarded-For': '192.168.0.91',
+        'X-Originating-IP': '192.168.0.92',
+        'X-Remote-IP': '192.168.0.93',
+        'X-Remote-Addr': '192.168.0.94',
+        'X-Client-IP': '192.168.0.95',
         'Host': 'www.1542727.com',
-        'Content-Length': '277',
         'Sec-Ch-Ua': '"Chromium";v="95", ";Not A Brand";v="99"',
-        'Accept': '*/*',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept': 'text/html, */*; q=0.01',
         'X-Requested-With': 'XMLHttpRequest',
         'Sec-Ch-Ua-Mobile': '?0',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
         'Sec-Ch-Ua-Platform': '"macOS"',
-        'Origin': 'https://www.1542727.com',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Dest': 'empty',
@@ -32,17 +36,60 @@ def create_user():
         'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
     }
     
+    #response = requests.get('https://www.1542727.com/login/registerTab', headers=get_csrf_headers, cookies=get_csrf_cookies, verify=False)
+    ## print(response.text)
+    #if response.status_code == 400:
+    #    print('請求街口異常')
+    #elif response.status_code == 504:
+    #    print('服務器異常')
+    #elif response.status_code == 403:
+    #    print('遭地區限制')
+    #elif response.status_code == 200:
+    #    response = response.text
+    #    # response = json.loads(response)
+    #    pattern = r'rf_cs_rForm_.*'
+    #    first_str = re.findall(pattern, response)[0]
+    #    csrf = first_str[23:-4]
+    #    csrf_encode = quote(csrf, 'utf-8')
+
+    cookies = {
+        '_csrf_token': 'cf7638812854d578dd6f03c6f4999f65bdff573c6fde9584ed9e424e542acc90',
+        'AWSALB': 'p08ovf3gODQj2li2a99eQLRwcAEN/mI/SkBdib8p9zT6FrPXx1Bc9ZVWZf77d3TRdJinObyQL0wIwmMAqzQ8X/GH8qRhgP3SGNS2W1VRVGiXPl3DTh6MS9AFua9k',
+        'AWSALBCORS': 'p08ovf3gODQj2li2a99eQLRwcAEN/mI/SkBdib8p9zT6FrPXx1Bc9ZVWZf77d3TRdJinObyQL0wIwmMAqzQ8X/GH8qRhgP3SGNS2W1VRVGiXPl3DTh6MS9AFua9k',
+        'PHPSESSID': '68qhsbek9lst3ouq3aq56h788c',
+        'my-application-browser-tab': '',
+    }
+
+    headers = {
+        'Host': 'www.1542727.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Length': '281',
+        'Origin': 'https://www.1542727.com',
+        'Connection': 'close',
+        'Referer': 'https://www.1542727.com/',
+    }
+    csrf_token = "e08fb2efaa1464b4b07a8ca458665fa4eRIn3NayygvZdyt9PsohBDJDuCCk7Rhc0SICEOQN16j0w2mBNKGhTrXn7R5qPfXsFlttpsTh9f34OxSetwGXzg%3D%3D"
     username = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz0123456789',10))
-    #data = 'username={0}&password=fdgfdgfdg&referral_code=&captcha=4777&cg_bind=0&cg_account_id=&cg_bind_from=register&rf_cs_rForm_=2BB3cm5zsTTAN3dWrx8U3SmKQvaEm2tvAHXJdieYlOvUwO7HpNA%3D%3D&password_confirmation=fdgfdgfdg'.format(username)
-    data = 'username={0}&password=fdgfdgfd&referral_code=&captcha=7934&cg_bind=0&cg_account_id=&cg_bind_from=register&rf_cs_rForm_=bae2de263ddef313489f8b1635f10d93wQfV3OaAoB9LBpr2v6CqWlJ3VjdBCdVbtgwfHD0MYB61X788x6Abf%2B376MbsKuhBxyJvYKM807vpb7%2B%2FG1pqZQ%3D%3D&password_confirmation=fdgfdgfd'.format(username)
+    data = 'username={0}&password=fdgfdgfd&referral_code=&captcha=3960&cg_bind=0&cg_account_id=&cg_bind_from=register&rf_cs_rForm_={1}&password_confirmation=fdgfdgfd'.format(username,csrf_token)
 
     response = requests.post('https://www.1542727.com/login/registerUser', headers=headers, cookies=cookies, data=data, verify=False)
+    now = datetime.datetime.now()
     if  response.status_code == 400 :
-        print('請求街口異常')
+        print('請求接口異常')
+        os.system("echo {1}   {0} 請求接口異常 >> /tmp/123.txt".format(response.status_code, now))
 
+    elif  response.status_code == 504 :
+        print('服務器異常')
+        os.system("echo {1}   {0} 服務器異常 >> /tmp/123.txt".format(response.status_code, now))
     elif  response.status_code == 403 :
         print('遭地區限制')
-    else:
+        os.system("echo {1}   {0} 遭地區限制 >> /tmp/123.txt".format(response.status_code, now))
+    elif response.status_code == 200:
         response = response.text
         response = json.loads(response)
         message = response['message']
@@ -53,7 +100,13 @@ def create_user():
         else:
             print(message,username)
             account_list.append(username)
+            os.system("echo {1}   200 {0}帳號創建成功 >> /tmp/123.txt".format(username,now))
+    else:
+        os.system("echo {1}   {0} 請求失敗,其他異常 >> /tmp/123.txt".format(response.status_code, now))
+        print(response.status_code)
+        print(response.text)
 
+        
 account_list = []
 def run(total_time,processes):
     with ThreadPoolExecutor(max_workers=processes) as executor:
@@ -61,8 +114,8 @@ def run(total_time,processes):
 
 
 start = time.time()
-total_time = 1000000
-processes = 1
+total_time = 10000000
+processes = 10
 run(total_time,processes)
 end = time.time()
 #print('開始時間',start)
